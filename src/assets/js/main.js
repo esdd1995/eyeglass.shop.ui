36c8c19f5b26eaa4b489a31a6ab323c96b8d3ccc
Emad <e.salamatmanesh@eppad.com>
2023-09-07 02:09:20 +0330

    fix navbar

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -47,44 +47,90 @@
     });
 
     $(document).ready(function () {
-        // Toggle the display of "#droplenz" when the link is clicked
-        $('#droplenz-link').click(function (e) {
-            e.preventDefault();
-            $('#droplenz').toggle();
-        });
-    });
-    $(document).ready(function () {
-        // Toggle the display of "#droplenz" when the link is clicked
-        $('#dropsun-link').click(function (e) {
-            e.preventDefault();
-            $('#dropsun').toggle();
-        });
+        var isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints; // Check if it's a touch device
+        var timeout;
+        var activeElement = null;
+        var isMouseInside = false;
 
-    });
-    $(document).ready(function () {
-        // Toggle the display of "#droplenz" when the link is clicked
-        $('#tamasi-link').click(function (e) {
-            e.preventDefault();
-            $('#tamasi').toggle();
-        });
-    });
-    $(document).ready(function () {
-        // Toggle the display of "#droplenz" when the link is clicked
-        $('#adasi-link').click(function (e) {
-            e.preventDefault();
-            $('#adasi').toggle();
-        });
+        // Function to show the element as a dialog and set z-index for children
+        function showDialog(elementId) {
+            $('#' + elementId).css({
+                'display': 'block',
+                'position': 'fixed', // Set the position to 'fixed'
+                'z-index': 1000, // Adjust the z-index as needed to appear above other elements
+                'pointer-events': 'auto' // Enable pointer events for elements inside the dialog
+            }).children().css('z-index', 1000); // Set z-index for children
+        }
 
+        // Function to hide the element and reset z-index for children
+        function hideElement(elementId) {
+            $('#' + elementId).css({
+                'display': 'none',
+                'pointer-events': 'none' // Disable pointer events for the dialog
+            }).children().css('z-index', 'auto'); // Reset z-index for children
+        }
+
+        // Function to handle mouseenter/mouseleave events for links and the dialog container
+        function handleMouseEvents(linkId, elementId) {
+            $('#' + linkId + ', #' + elementId).mouseenter(function () {
+                clearTimeout(timeout);
+                isMouseInside = true;
+                if (activeElement !== elementId) {
+                    hideElement(activeElement); // Hide the previous dialog if any
+                    showDialog(elementId);
+                    activeElement = elementId;
+                }
+            });
+
+            $('#' + linkId + ', #' + elementId).mouseleave(function () {
+                isMouseInside = false;
+                timeout = setTimeout(function () {
+                    if (!isMouseInside) {
+                        hideElement(elementId);
+                        activeElement = null;
+                    }
+                }, 500); // Adjust the delay as needed
+            });
+        }
+
+        // Call the functions for each link and element
+        handleMouseEvents('droplenz-link', 'droplenz');
+        handleMouseEvents('dropsun-link', 'dropsun');
+        handleMouseEvents('tamasi-link', 'tamasi');
+        handleMouseEvents('adasi-link', 'adasi');
+        // Add more links and elements as needed
+
+        // Mouseenter/mouseleave for non-touch devices
+        if (!isTouchDevice) {
+            $('#droplenz-link, #dropsun-link, #tamasi-link, #adasi-link, #filter-link').mouseenter(function () {
+                clearTimeout(timeout);
+                var elementId = $(this).attr('id').replace('-link', '');
+                showElement(elementId);
+            });
+
+            $('#droplenz, #dropsun, #tamasi, #adasi, #filter').mouseenter(function () {
+                clearTimeout(timeout);
+            });
+
+            $('#droplenz-link, #dropsun-link, #tamasi-link, #adasi-link, #filter-link').mouseleave(function () {
+                var elementId = $(this).attr('id').replace('-link', '');
+                timeout = setTimeout(function () {
+                    hideElement(elementId);
+                }, 500); // Adjust the delay as needed
+            });
+
+            $('#droplenz, #dropsun, #tamasi, #adasi, #filter').mouseleave(function () {
+                timeout = setTimeout(function () {
+                    hideElement('droplenz');
+                    hideElement('dropsun');
+                    hideElement('tamasi');
+                    hideElement('adasi');
+                    hideElement('filter');
+                }, 500); // Adjust the delay as needed
+            });
+        }
     });
 
-    $(document).ready(function () {
-        // Toggle the display of "#droplenz" when the link is clicked
-        $('#filter-link').click(function (e) {
-            e.preventDefault();
-            $('#filter').toggle();
-        });
-
-    });
 
 
     //faq
@@ -131,9 +177,9 @@
     // Testimonials carousel
     $(document).ready(function () {
         setTimeout(() => {
-            
+
         }, 5000);
-       
+
     });
 
     // Modal Video
